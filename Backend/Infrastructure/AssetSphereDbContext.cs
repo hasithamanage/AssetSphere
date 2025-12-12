6ee990ed1db96baaedc2348480e69fb72c503f0d
Hasitha Manage <hasithahimakara@gmail.com>
2025-12-12 15:48:17 +0200

    chore: initial backend project structure

---
+++ Backend/Infrastructure/AssetSphereDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using AssetSphere.Domain.Entities;
+
+namespace AssetSphere.Infrastructure
+{
+    public class AssetSphereDbContext : DbContext
+    {
+        public AssetSphereDbContext(DbContextOptions<AssetSphereDbContext> options)
+            : base(options)
+        {
+        }
+
+        // DbSets for your entities
+        public DbSet<Asset> Assets { get; set; }
+        public DbSet<WorkOrder> WorkOrders { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Configure entity mappings here if needed
+        }
+    }
+}
